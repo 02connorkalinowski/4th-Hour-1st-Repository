f79494462f08f360734299a1b829c441cb0639b8
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-02-20 08:16:40 -0800

    Add Enter a number using Try and Except rules

---
+++ Exceptions-Git.py
@@ -0,0 +1,11 @@
+# 4.16.3: Enter a Number using Try and Except
+# Connor
+# 2.20.19
+
+try:
+	my_num = int(input('Enter an interger: '))
+	print('Your number:', my_num)
+
+except ValueError:
+	print('\n''That was not an Interger!')
+'''
