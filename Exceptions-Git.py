96765bae373fce6f9a6a01c13b9d20e4d6aa0030
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-02-20 08:30:10 -0800

    Add Enter a name and Age excets

--- Exceptions-Git.py
+++ Exceptions-Git.py
@@ -1,11 +1,28 @@
 # 4.16.3: Enter a Number using Try and Except
 # Connor
 # 2.20.19
+#
+# try:
+# 	my_num = int(input('Enter an interger: '))
+# 	print('Your number:', my_num)
+#
+# except ValueError:
+# 	print('\n''That was not an Interger!')
+
+
+
+# 4.16.4: Enter Name & Age using the Try & Except Rule
+# Connor
+# 2.20.19
+
+name = input('Enter your name: ')
+
+age = -1
 
 try:
-	my_num = int(input('Enter an interger: '))
-	print('Your number:', my_num)
-
+	age = int(input('Enter your age: '))
 except ValueError:
-	print('\n''That was not an Interger!')
-'''
+	print('\n''That was not an Integer for your age')
+
+print('Name: ', name)
+print('Age:', age)
