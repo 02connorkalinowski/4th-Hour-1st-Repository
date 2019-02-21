a4ad7ec3d02de5f13280b3e478dc8fbe87ca36e2
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-02-21 09:45:22 -0800

    Add Enter a Positive number

--- Exceptions-Git.py
+++ Exceptions-Git.py
@@ -30,18 +30,35 @@
 # 4.16.6: Temperature Converter using Try and Except
 # Connor
 # 2.20.19
-def celcius_to_fahrenheit(celcius):
-	return celcius * 1.8 + 32
+# def celcius_to_fahrenheit(celcius):
+# 	return celcius * 1.8 + 32
+#
+# def fahrenheit_to_celcius (fahrenhei):
+# 	return (fahrenheit - 32) / 1.8
+#
+# try:
+# 	c = float(input('Enter a Temp in Celcius: '))
+# 	print('In F:', round(celcius_to_fahrenheit(c), 2))
+#
+# 	f = float(input('Enter a Temp in Fahrenheit: '))
+# 	print('In C:', round(fahrenheit_to_celcius(f), 2))
+#
+# except ValueError:
+# 	print('You must Enter a Float!')
 
-def fahrenheit_to_celcius (fahrenhei):
-	return (fahrenheit - 32) / 1.8
 
-try:
-	c = float(input('Enter a Temp in Celcius: '))
-	print('In F:', round(celcius_to_fahrenheit(c), 2))
+#4.16.7: Enter a Positive Number
+# Connor
+# 2.21.19
 
-	f = float(input('Enter a Temp in Fahrenheit: '))
-	print('In C:', round(fahrenheit_to_celcius(f), 2))
-
-except ValueError:
-	print('You must Enter a Float!')
+def retrieve_positive_number():
+	while True:
+		try:
+			number = int(input('Enter a Positive number: '))
+			if number > 0:
+				return number
+			else:
+				print('The number must be positive, silly.')
+		except ValueError:
+			print('That was not a number.')
+retrieve_positive_number()
