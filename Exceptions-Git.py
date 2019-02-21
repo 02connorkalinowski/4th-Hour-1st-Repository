14491af83cbe2c1851573008f016f4fec087d69f
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-02-21 09:26:43 -0800

    Add temp convert using try and except

--- Exceptions-Git.py
+++ Exceptions-Git.py
@@ -15,14 +15,33 @@
 # Connor
 # 2.20.19
 
-name = input('Enter your name: ')
+# name = input('Enter your name: ')
+#
+# age = -1
+#
+# try:
+# 	age = int(input('Enter your age: '))
+# except ValueError:
+# 	print('\n''That was not an Integer for your age')
+#
+# print('Name: ', name)
+# print('Age:', age)
 
-age = -1
+# 4.16.6: Temperature Converter using Try and Except
+# Connor
+# 2.20.19
+def celcius_to_fahrenheit(celcius):
+	return celcius * 1.8 + 32
+
+def fahrenheit_to_celcius (fahrenhei):
+	return (fahrenheit - 32) / 1.8
 
 try:
-	age = int(input('Enter your age: '))
-except ValueError:
-	print('\n''That was not an Integer for your age')
+	c = float(input('Enter a Temp in Celcius: '))
+	print('In F:', round(celcius_to_fahrenheit(c), 2))
 
-print('Name: ', name)
-print('Age:', age)
+	f = float(input('Enter a Temp in Fahrenheit: '))
+	print('In C:', round(fahrenheit_to_celcius(f), 2))
+
+except ValueError:
+	print('You must Enter a Float!')
